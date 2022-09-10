64e7a0b2d08c4ad5de823924c3c954bce580d033
progressng <progressigbinosa@gmail.com>
2022-09-10 00:14:22 -0700

    Prints last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,13 +8,26 @@
  * Return: 0
  */
 
-int main(void)
+int main(voi)
 {
-	char ch;
-	for (ch = 'a' ; ch = 'z' ; ch++)
+	int n;
+	int l;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	l = n % 10;
+
+	if (l > 5)
 	{
-			putchar(ch);
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
-	putchar('\n');
+	elseif (l == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l);
+	}
+	else if (l < 6 && l != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	}
 	return (0);
 }
