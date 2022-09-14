a9b734731bae0fd2b707a29883e8fed934f6dfb5
progressng <progressigbinosa@gmail.com>
2022-09-14 13:43:58 -0700

    Lowercase alphaabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/** 
+ * Print alphabet - Function to print lowercase alphabets
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
+}
