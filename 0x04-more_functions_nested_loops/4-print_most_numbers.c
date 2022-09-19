442309b9e0b00ec8958e78e377d339a330a2a01e
progressng <progressigbinosa@gmail.com>
2022-09-19 10:49:45 -0700

    Print numbers between 0 to 9, except 2 and 3

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0 to 9, except 2 and 3
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		if (a != 2 && a != 4)
+		{
+			_putchar(a + '0');
+		}
+		a++;
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -13,3 +13,5 @@ void print_triangle(int size);
 void print_number(int n);
 
 void more_numbers(void);
+
+void print_most_numbers(void);
