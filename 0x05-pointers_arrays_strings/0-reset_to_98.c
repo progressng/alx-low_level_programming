8bc2f47c016aed6098b467986bd24ed8decbd923
progressng <progressigbinosa@gmail.com>
2022-09-19 11:42:35 -0700

    Updates value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to nd changes to 98
+ * @n: pointer to the variable to be updated
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
