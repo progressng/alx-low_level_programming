ab7179087ae88987e66271f762f5b545baedcb56
progressng <progressigbinosa@gmail.com>
2022-09-19 08:46:40 -0700

    function that prints an integer

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+int _putchar(char c);
+
 int _isupper(int c);
 
 int _isdigit(int c);
@@ -5,3 +7,7 @@ int _isdigit(int c);
 int mul(int a, int b);
 
 void print_numbers(void);
+
+void print_triangle(int size);
+
+void print_number(int n);
