dffe3c209d3a87038a6af481ef23913479b0ff04
progressng <progressigbinosa@gmail.com>
2022-09-22 21:33:03 -0700

    String encoding in 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string in 1337
+ * @s: string to be encoded
+ * Return: the resultant string
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char *a = "aAeEo0tTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+	return (s);
+}
