36fa26ee31fd24e57c0a417ff615543deb5c7f40
progressng <progressigbinosa@gmail.com>
2022-10-27 20:19:01 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endiannedd
+ *
+ * Return: if big-endian - 0.
+ * If little-endian - 1.
+ */
+
+int get_endianness(void)
+{
+	int num = 1;
+	char *endian = (char *)&num;
+
+	if (*endian == 1)
+		return (1);
+	return (0);
+}
